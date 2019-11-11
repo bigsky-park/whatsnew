de0b2b44762003636bc700d9539dc75f12be39ed
bigsky-park <ptm302@gmail.com>
2019-11-11 22:30:33 +0900

    Initialize skeleton of project

---
+++ web/src/main/java/kr/bigskypark/whatsnew/web/config/WebConfig.java
@@ -0,0 +1,10 @@
+package kr.bigskypark.whatsnew.web.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.EnableWebFlux;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+@Configuration
+@EnableWebFlux
+public class WebConfig implements WebFluxConfigurer {
+}
---
+++ web/src/main/java/kr/bigskypark/whatsnew/web/router/HelloRouter.java
@@ -0,0 +1,21 @@
+package kr.bigskypark.whatsnew.web.router;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+
+@Configuration
+public class HelloRouter {
+
+    @Bean
+    public RouterFunction<ServerResponse> hello() {
+        return RouterFunctions
+                .route(GET("/hello"),
+                        request -> ServerResponse.ok().bodyValue("hello"));
+    }
+
+}
